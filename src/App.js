348da4bd4662467513c2de32e275f23c31f75896
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-03-17 12:02:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+
+// API KEy: 60715b35
+
+const API_URL = 'https://www.omdbapi.com?apikey=60715b35';
+
 function App() {
+
+  const SearchMOvies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(()=>{
+    SearchMOvies("Spiderman");
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>App</h1>
     </div>
   );
 }
